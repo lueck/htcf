560697e12c661524c8200d72010907ba770173a7
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-01-16 17:22:28 +0100

    Moved propagateOffsets to TcfElement.hs

--- src/Text/XML/TCF/Parser/TcfElement.hs
+++ src/Text/XML/TCF/Parser/TcfElement.hs
@@ -2,6 +2,7 @@ module Text.XML.TCF.Parser.TcfElement
   ( TcfElement (..)
   , tcfTextLen
   , dupWithNewTextPos
+  , propagateOffsets
   )
   where
 
@@ -31,4 +32,12 @@ dupWithNewTextPos (PositionedText tx _ xOffset) i =
   PositionedText tx i xOffset
 dupWithNewTextPos (PositionedStructure qN _ _ xStart xEnd) i =
   PositionedStructure qN i 0 xStart xEnd
+
+propagateOffsets :: [TcfElement] -> [TcfElement]
+propagateOffsets xs = propagateOffsets' 0 xs
+  where
+    propagateOffsets' :: TextPosition -> [TcfElement] -> [TcfElement]
+    propagateOffsets' i (x:[]) = [dup x i]
+    propagateOffsets' i (x:xs) = dup x i : propagateOffsets' (i+(tcfTextLen x)+1) xs
+    dup = dupWithNewTextPos
 
--- src/Text/XML/TCF/Parser/TextLayerParser.hs
+++ src/Text/XML/TCF/Parser/TextLayerParser.hs
@@ -1,20 +1,11 @@
 module Text.XML.TCF.Parser.TextLayerParser
-  ( propagateOffsets
-  , mkTcfText
+  ( mkTcfText
   ) where
 
 import Text.XML.HXT.Core
 
 import Text.XML.TCF.Parser.Position
 import Text.XML.TCF.Parser.TcfElement
-
-propagateOffsets :: [TcfElement] -> [TcfElement]
-propagateOffsets xs = propagateOffsets' 0 xs
-  where
-    propagateOffsets' :: TextPosition -> [TcfElement] -> [TcfElement]
-    propagateOffsets' i (x:[]) = [dup x i]
-    propagateOffsets' i (x:xs) = dup x i : propagateOffsets' (i+(tcfTextLen x)+1) xs
-    dup = dupWithNewTextPos
 
 mkTcfText :: IOSArrow XmlTree TcfElement
 mkTcfText =
