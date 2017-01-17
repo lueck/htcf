97bc57490603fa99ca48734843fe7326b23e26c2
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-01-17 11:54:04 +0100

    Fixed incrementation in propagateOffsets.
    - Do not increment for structure elements etc.
    - Verification of incrementation is still needed.

--- src/Text/XML/TCF/Parser/TcfElement.hs
+++ src/Text/XML/TCF/Parser/TcfElement.hs
@@ -38,6 +38,9 @@ propagateOffsets xs = propagateOffsets' 0 xs
   where
     propagateOffsets' :: TextPosition -> [TcfElement] -> [TcfElement]
     propagateOffsets' i (x:[]) = [dup x i]
-    propagateOffsets' i (x:xs) = dup x i : propagateOffsets' (i+(tcfTextLen x)+1) xs
+    propagateOffsets' i (x:xs) = dup x i : propagateOffsets' (i+(tcfTextLen x)+(incValue x)) xs
     dup = dupWithNewTextPos
-
+    -- Increment by 1 for PositionedText, but 0 for others
+    -- FIXME: Do we really need this incrementation? Verification needed!
+    incValue (PositionedText _ _ _) = 1
+    incValue _ = 0
