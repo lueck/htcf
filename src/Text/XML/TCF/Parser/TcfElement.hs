61cc8a8028f49bb8f962bd46591c67b2dfdf8b3f
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-01-23 00:50:23 +0100

    Introduced TcfLineBreak for controling the tokenizer

--- src/Text/XML/TCF/Parser/TcfElement.hs
+++ src/Text/XML/TCF/Parser/TcfElement.hs
@@ -2,6 +2,7 @@ module Text.XML.TCF.Parser.TcfElement
   ( TcfElement (..)
   , isTcfText
   , isTcfStructure
+  , isTcfLineBreak
   , tcfTextLen
   , dupWithNewTextPos
   , propagateOffsets
@@ -25,7 +26,9 @@ data TcfElement =
   , textEnd :: TextPosition        -- ^ end position in text layer
   , srcStart :: XmlPosition        -- ^ start position in source
   , srcEnd :: XmlPosition          -- ^ end position in end
-  } deriving (Show)
+  }
+  | TcfLineBreak                   -- ^ control sign for the tokenizer
+  deriving (Show)
 
 isTcfText :: TcfElement -> Bool
 isTcfText (TcfText _ _ _) = True
@@ -34,6 +37,10 @@ isTcfText _ = False
 isTcfStructure :: TcfElement -> Bool
 isTcfStructure (TcfStructure _ _ _ _ _) = True
 isTcfStructure _ = False
+
+isTcfLineBreak :: TcfElement -> Bool
+isTcfLineBreak (TcfLineBreak) = True
+isTcfLineBreak _ = False
 
 tcfTextLen :: TcfElement -> Int
 tcfTextLen (TcfText t _ _) = length t
@@ -44,12 +51,13 @@ dupWithNewTextPos (TcfText tx _ xOffset) i =
   TcfText tx i xOffset
 dupWithNewTextPos (TcfStructure qN _ _ xStart xEnd) i =
   TcfStructure qN i 0 xStart xEnd
+dupWithNewTextPos (TcfLineBreak) _ = TcfLineBreak
 
 propagateOffsets :: [TcfElement] -> [TcfElement]
 propagateOffsets xs = propagateOffsets' 0 xs
   where
     propagateOffsets' :: TextPosition -> [TcfElement] -> [TcfElement]
-    propagateOffsets' i [] = []
+    propagateOffsets' _ [] = []
     propagateOffsets' i (x:[]) = [dup x i]
     propagateOffsets' i (x:xs) = dup x i : propagateOffsets' (i+(tcfTextLen x)+(incValue x)) xs
     dup = dupWithNewTextPos
@@ -61,3 +69,4 @@ propagateOffsets xs = propagateOffsets' 0 xs
 serialize :: TcfElement -> String
 serialize (TcfText t _ _) = t
 serialize (TcfStructure qN _ _ _ _) = "{{ " ++ show qN ++ " }}"
+serialize (TcfLineBreak) = "$LineBreak$"
