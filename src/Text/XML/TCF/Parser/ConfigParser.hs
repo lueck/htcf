9b6ba0f68d0db9d8b2907446a74c0387fc740d7e
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-01-22 01:31:59 +0100

    Config Parser: Parsing for more configuration aspects.
    - Added TextRoot, LineBreak and Hyphen for the Tokenizer.
    
    - Unittest for the changes.

--- src/Text/XML/TCF/Parser/ConfigParser.hs
+++ src/Text/XML/TCF/Parser/ConfigParser.hs
@@ -1,28 +1,102 @@
+-- | A config file will be parsed by 'runConfigParser' into a list of
+-- @['Config']@. Use 'getTextRoot', 'getHyphens', etc. to get an
+-- aspect of the configuration from this list.
+
 module Text.XML.TCF.Parser.ConfigParser
-  ( stripped
+  ( Config (..)
   , runConfigParser
+  , getTextRoot
+  , getHyphens
+  , getLineBreaks
+  , getDroppedTrees
   ) where
 
 import Text.XML.HXT.Core
 
-stripped :: FilePath -> IOSArrow XmlTree QName
-stripped fname =
-  readDocument [withValidate no] fname >>>
-  getChildren >>>
-  isElem >>> hasName "config" >>>
-  getChildren >>>
-  isElem >>> hasName "strippedText" >>>
-  getChildren >>>
-  isElem >>> hasName "simpleElement" >>> strippedQName
+data Config =
+  TextRoot QName                        -- ^ qname of parent node for text
+  | DroppedTree                         -- ^ xml subtree to be dropped by the parser
+  { qName :: QName }                    -- ^ qname of element node
+  | LineBreak QName                     -- ^ qname of element
+                                        -- resulting in a control sign
+                                        -- for the tokenizer
+  | Hyphen Char                         -- ^ hyphen character for tokenizer
+  deriving (Show, Eq)
 
-strippedQName :: IOSArrow XmlTree QName
-strippedQName =
+-- | Returns the maybe qualified named of the text root defined in the
+-- config. If there where multiple text roots defined in the config
+-- file, the qualified name of the first definition and only this one
+-- is returned.
+getTextRoot :: [Config] -> Maybe QName
+getTextRoot [] = Nothing
+getTextRoot ((TextRoot qn):_) = Just qn
+getTextRoot (_:xs) = getTextRoot xs
+
+-- | Returns a list of hyphen characters defined in the
+-- config. Cf. 'Hyphen'.
+getHyphens :: [Config] -> [Char]
+getHyphens [] = []
+getHyphens ((Hyphen c):xs) = c : getHyphens xs
+getHyphens (_:xs) = getHyphens xs
+
+-- | Returns a list of the qualified names of element nodes, that
+-- produce linebreak control characters for the
+-- tokenizer. Cf. 'LineBreak'.
+getLineBreaks :: [Config] -> [QName]
+getLineBreaks [] = []
+getLineBreaks ((LineBreak qn):xs) = qn : getLineBreaks xs
+getLineBreaks (_:xs) = getLineBreaks xs
+
+-- | Returns a list of qualified names of element nodes, that are
+-- dropped by the parser. Cf. 'DroppedTree'.
+getDroppedTrees :: [Config] -> [QName]
+getDroppedTrees [] = []
+getDroppedTrees ((DroppedTree qn):xs) = qn : getDroppedTrees xs
+getDroppedTrees (_:xs) = getDroppedTrees xs
+
+-- | An arrow for parsing the config file.
+parseConfig :: IOSArrow XmlTree Config
+parseConfig =
+  (hasName "textRoot" >>> textRoot)
+  <+>
+  (hasName "droppedTree" >>> getChildren >>>
+  hasName "simpleElement" >>> droppedTreeSimple)
+  <+>
+  (hasName "tokenizerControl" >>> getChildren >>>
+  hasName "linebreak" >>> linebreak)
+  <+>
+  (hasName "specialCharacter" >>> getChildren >>>
+  hasName "hyphen" >>> hyphen)
+
+textRoot :: IOSArrow XmlTree Config
+textRoot =
   getAttrValue0 "namespace" &&&
   getAttrValue0 "name" >>>
-  arr (uncurry mkNsName)
+  arr (TextRoot . (uncurry mkNsName))
 
-runConfigParser :: FilePath -> IO [QName]
+droppedTreeSimple :: IOSArrow XmlTree Config
+droppedTreeSimple =
+  getAttrValue0 "namespace" &&&
+  getAttrValue0 "name" >>>
+  arr (DroppedTree . (uncurry mkNsName))
+
+linebreak :: IOSArrow XmlTree Config
+linebreak =
+  getAttrValue0 "namespace" &&&
+  getAttrValue0 "name" >>>
+  arr (LineBreak . (uncurry mkNsName))
+
+hyphen :: IOSArrow XmlTree Config
+hyphen =
+  getAttrValue0 "char" >>>
+  arr (Hyphen . head)
+
+-- | Returns the config from a file. 
+runConfigParser :: FilePath -> IO [Config]
 runConfigParser fname = do
-  results <- runX (stripped fname)
+  results <- runX (readDocument [withValidate no] fname >>>
+                   propagateNamespaces //>
+                   hasName "config" >>>
+                   multi parseConfig)
   return results
 
--- testsuite/Test/Text/XML/TCF/Parser/ConfigParser.hs
+++ testsuite/Test/Text/XML/TCF/Parser/ConfigParser.hs
@@ -5,12 +5,18 @@ module Test.Text.XML.TCF.Parser.ConfigParser where
 import Test.Framework
 import Text.XML.HXT.Core
 
-import Text.XML.TCF.Parser.ConfigParser (stripped)
+import Text.XML.TCF.Parser.ConfigParser
 
 configFile = "testsuite/Test/Text/XML/TCF/Parser/config.xml"
 
 test_stripped = do
-  results <- runX (stripped configFile)
+  results <- runConfigParser configFile
   assertEqual
-    [mkNsName "http://www.tei-c.org/ns/1.0" "fw"]
+    [(TextRoot (mkNsName "http://www.tei-c.org/ns/1.0" "text")),
+     (DroppedTree (mkNsName "http://www.tei-c.org/ns/1.0" "fw")),
+     (LineBreak (mkNsName "http://www.tei-c.org/ns/1.0" "lb")),
+     (LineBreak (mkNsName "http://www.tei-c.org/ns/1.0" "l")),
+     (Hyphen '-'),
+     (Hyphen '\172')]
     results
+
