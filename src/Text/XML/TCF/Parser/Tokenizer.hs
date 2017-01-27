bf9e88e2e87e2937539b4c3d3ede259d1f4d7be5
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-01-27 22:00:10 +0100

    Tokenizer: provide IDs (numbers)

--- src/Text/XML/TCF/Parser/Tokenizer.hs
+++ src/Text/XML/TCF/Parser/Tokenizer.hs
@@ -14,15 +14,18 @@ import Text.XML.TCF.Parser.ConfigParser
 data Token =
   Token
   { token :: String
-  , textOffset :: TextPosition
-  , srcOffset :: XmlPosition
+  , tokenId :: Maybe Int
+  , start :: Maybe TextPosition
+  , end :: Maybe TextPosition
+  , srcStart :: Maybe XmlPosition
+  , srcEnd :: Maybe XmlPosition
   } deriving Show
 
 -- | @tokenize@ is the tokenizer function. It takes a configuration
 -- (cf. 'Config') as first parameter and a list of 'TcfElement's as
 -- second parameters. It returns a list of 'Token's.
 tokenize :: [Config] -> [TcfElement] -> [Token]
-tokenize cfg tcf = tokenize' tcf
+tokenize cfg tcf = tokenize' 1 tcf
   where
     -- We call _break_ a separating character.
     isBreak :: Char -> Bool
@@ -33,36 +36,38 @@ tokenize cfg tcf = tokenize' tcf
     hasBreak s = isJust $ find isBreak s
 
     -- tokenize' does all the work.
-    tokenize' :: [TcfElement] -> [Token]
-    tokenize' [] = []
+    tokenize' :: Int -- ^ token id (number)
+              -> [TcfElement] -- ^ tcf elements left over
+              -> [Token] -- ^ tokens returned
+    tokenize' _ [] = []
 
     -- Drop Structure: when at 1st, 2nd, 3rd, 4th position. -- This
     -- should go as far as needed for the patterns below, which must
     -- not be interleaved with TcfStructures.
-    tokenize' ((TcfStructure _ _ _ _ _):xs) = tokenize' xs
-    tokenize' (x:(TcfStructure _ _ _ _ _):xs) = tokenize' (x:xs)
-    tokenize' (x:x2:(TcfStructure _ _ _ _ _):xs) = tokenize' (x:x2:xs)
-    tokenize' (x:x2:x3:(TcfStructure _ _ _ _ _):xs) = tokenize' (x:x2:x3:xs)
+    tokenize' i ((TcfStructure _ _ _ _ _):xs) = tokenize' i xs
+    tokenize' i (x:(TcfStructure _ _ _ _ _):xs) = tokenize' i (x:xs)
+    tokenize' i (x:x2:(TcfStructure _ _ _ _ _):xs) = tokenize' i (x:x2:xs)
+    tokenize' i (x:x2:x3:(TcfStructure _ _ _ _ _):xs) = tokenize' i (x:x2:x3:xs)
     
     -- Continueing Token: A continuing token spans more than one
     -- TcfText without break.
-    tokenize' ((TcfText t1 tOffset1 sOffset1) : (TcfText (t2:t2s) tOffset2 sOffset2) : xs)
+    tokenize' i ((TcfText t1 tOffset1 sOffset1) : (TcfText (t2:t2s) tOffset2 sOffset2) : xs)
       | (not $ null t1) && -- First text is non-empty.
         (not $ hasBreak t1) && -- First text is a token or less.
         (not $ isBreak t2) -- Text 2 does not start with break.
       -- We do not create a token, but move letters from the second
       -- text to the first. This will be repeated for tokens that span
       -- several text nodes.
-      = tokenize' ((TcfText
-                    (t1 ++ take letters (t2:t2s))
-                    tOffset1
-                    {-sOffset1 FIXME-} 666)
-                   :
-                   (TcfText
-                    (drop letters (t2:t2s))
-                    (shiftTextPosition tOffset2 letters)
-                    (shiftTextPosition sOffset2 letters))
-                   : xs)
+      = tokenize' i ((TcfText
+                      (t1 ++ take letters (t2:t2s))
+                      tOffset1
+                      {-sOffset1 FIXME-} 666)
+                     :
+                     (TcfText
+                      (drop letters (t2:t2s))
+                      (shiftTextPosition tOffset2 letters)
+                      (shiftTextPosition sOffset2 letters))
+                     : xs)
       where
         letters = length $ takeWhile (not . isBreak) (t2:t2s)
 
@@ -75,12 +80,12 @@ tokenize cfg tcf = tokenize' tcf
     -- white space, iii) a TcfLineBreak, iv) the next text or
     -- structure elements. Here we just drop the white space (ii), so
     -- that this case is turned into Case 2.
-    tokenize' ((TcfText t1 tOffset1 sOffset1):(TcfText t2 _ _):(TcfLineBreak):xs)
+    tokenize' i ((TcfText t1 tOffset1 sOffset1):(TcfText t2 _ _):(TcfLineBreak):xs)
       | (not $ null t1) && -- first text must be non-empty
         (not $ hasBreak t1) && -- may there be trailing whitespace?
         (elem (last t1) $ getHyphens cfg) && -- trailing hyphen in t1
         (null $ dropWhile isSpace t2) -- drop white space node after linebreak
-      = tokenize' ((TcfText t1 tOffset1 sOffset1):(TcfLineBreak):xs)
+      = tokenize' i ((TcfText t1 tOffset1 sOffset1):(TcfLineBreak):xs)
 
     -- Hyphenation Case 2: Lines are separated by an empty line break
     -- tag, e.g. <TEI:lb/> or <xhtml:br/>. This results in i) a
@@ -96,46 +101,50 @@ tokenize cfg tcf = tokenize' tcf
     -- a newline character (and other white space) and the next
     -- letters where enclosed in a structure element,
     -- like in "diffe-<lb/>\n<em>rance</em>".
-    tokenize' ((TcfText t1 tOffset1 sOffset1):(TcfLineBreak):(TcfText t2 tOffset2 sOffset2):xs)
+    tokenize' i ((TcfText t1 tOffset1 sOffset1):(TcfLineBreak):(TcfText t2 tOffset2 sOffset2):xs)
       | (not $ null t1) && -- first text must be non-empty
         (not $ hasBreak t1) && -- first text is a word. May there be trailing whitespace?
         (elem (last t1) $ getHyphens cfg) -- trailing hyphen in first text
-      = tokenize' ((TcfText
-                    ((init t1) ++ (take letters $ drop spaces t2))
-                    tOffset1
-                    sOffset1)
-                   :
-                   (TcfText
-                    (drop (letters+spaces) t2)
-                    (shiftTextPosition tOffset2 (letters+spaces))
-                    (shiftXmlPosition sOffset2 (letters+spaces)))
-                   : xs)
+      = tokenize' i ((TcfText
+                      ((init t1) ++ (take letters $ drop spaces t2))
+                      tOffset1
+                      sOffset1)
+                     :
+                     (TcfText
+                      (drop (letters+spaces) t2)
+                      (shiftTextPosition tOffset2 (letters+spaces))
+                      (shiftXmlPosition sOffset2 (letters+spaces)))
+                     : xs)
       where
         spaces = length $ takeWhile isSpace t2
         letters = length $ takeWhile (not . isBreak) (drop spaces t2)
 
     -- Drop linebreaks that were not preceded by a hyphen
-    tokenize' ((TcfLineBreak):xs) = tokenize' xs
+    tokenize' i ((TcfLineBreak):xs) = tokenize' i xs
 
     -- Drop zero length text node.
-    tokenize' ((TcfText "" _ _):xs)
-      = tokenize' xs
+    tokenize' i ((TcfText "" _ _):xs)
+      = tokenize' i xs
 
     -- Make tokens by eating a single TcfText element. We prefer
     -- pattern matching with destructuring the text into (t:ts) over
     -- getting the head of the text in a guard.
-    tokenize' ((TcfText (t:ts) tOffset sOffset):xs)
+    tokenize' i ((TcfText (t:ts) tOffset sOffset):xs)
       -- Drop heading space
       | isSpace t
-      = tokenize' ((TcfText
-                    (drop spaces ts)
-                    (shiftTextPosition tOffset spaces+1)
-                    (shiftXmlPosition sOffset spaces+1))
-                   : xs)
+      = tokenize' i ((TcfText
+                      (drop spaces ts)
+                      (shiftTextPosition tOffset spaces+1)
+                      (shiftXmlPosition sOffset spaces+1))
+                     : xs)
       -- Punctuation token
       | isPunctuation t
-      = (Token (t:[]) tOffset sOffset)
-        : (tokenize'
+      = (Token
+         (t:[])
+         (Just i)
+         (Just tOffset) Nothing
+         (Just sOffset) Nothing)
+        : (tokenize' (i+1)
            ((TcfText
              ts
              (shiftTextPosition tOffset 1)
@@ -144,8 +153,12 @@ tokenize cfg tcf = tokenize' tcf
       -- Word token. Letters here means letters, digits, marks etc., but
       -- neither spaces nor punctuation.
       | otherwise
-      = (Token (t:(take letters ts)) tOffset sOffset)
-        : (tokenize'
+      = (Token
+         (t:(take letters ts))
+         (Just i)
+         (Just tOffset) Nothing
+         (Just sOffset) Nothing)
+        : (tokenize' (i+1)
            ((TcfText
              (drop letters ts)
              (shiftTextPosition tOffset letters+1)
