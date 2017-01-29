44a5e58d3e1b59fce5765ae62a595a0e5e71b307
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-01-30 00:05:18 +0100

    Write Tcf: functions for writing tokens
    - tests still needed

--- src/HTCF/ReadTcf.hs
+++ src/HTCF/ReadTcf.hs
@@ -3,9 +3,12 @@ module HTCF.ReadTcf
   , parseTcfLayers
   , parseTokens
   , parseToken
+  , writeTokens
+  , writeToken
   ) where
 
 import Text.XML.HXT.Core
+import Data.Maybe
 
 import HTCF.Layers
 import HTCF.Utils
@@ -49,3 +52,20 @@ parseToken cfg =
            (readIntMaybe $ Just sS)
            (readIntMaybe $ Just sE)))
 {-# INLINE parseToken #-}
+
+writeTokens :: (ArrowXml a) => [Config] -> [Layer] -> a XmlTree XmlTree
+writeTokens cfg ts =
+  (mkqelem
+   (mkNsName "tokens" $ getTcfTextCorpusNamespace cfg) -- qname
+   [] -- attribute nodes
+   (map (writeToken cfg) ts))
+
+writeToken :: (ArrowXml a) => [Config] -> Layer -> a XmlTree XmlTree
+writeToken cfg (Token t idd start end sStart sEnd) =
+  (mkqelem
+   (mkNsName "token" ns)
+   ((maybeAttr "id" idd) ++ (maybeAttr "start" start) ++ (maybeAttr "end" end) ++ (maybeAttr "srcStart" sStart) ++ (maybeAttr "srcEnd" sEnd))
+   [(txt t)])
+  where
+    ns = getTcfTextCorpusNamespace cfg
+    maybeAttr n val = maybeToList $ fmap ((sattr n) . show) val
