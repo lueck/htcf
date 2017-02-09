0da068c8f928451ce5d734547e3a1a54e5ebab4d
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2017-02-09 18:53:45 +0100

    Fixed unit tests for tokenizer
    - Tests were broken because constructor TcfText changed.
    
    - Tests for text offsets still broken

--- testsuite/Test/HTCF/Tokenizer.hs
+++ testsuite/Test/HTCF/Tokenizer.hs
@@ -5,6 +5,7 @@ module Test.HTCF.Tokenizer where
 import Test.Framework
 import Test.QuickCheck.Monadic
 import Text.XML.HXT.Core
+import Text.XML.HXT.Arrow.XmlState.RunIOStateArrow (runXIOState, initialState)
 import Data.Maybe
 
 import HTCF.Tokenizer
@@ -12,19 +13,28 @@ import HTCF.ConfigParser
 import HTCF.TcfParser
 import HTCF.TcfParserTypeDefs
 import HTCF.LayerTypeDefs
+import HTCF.LineOffsets
 
 sampleFile = "doc/examples/kant_aufklaerung_1784.TEI-P5.xml"
 
 monthsConfig = [(Month "Oktober")]
 
+mkTcfTextSample :: String -> Int -> Int -> TcfElement
+mkTcfTextSample t tOffset xOffset =
+  (TcfText t tOffset (Just xOffset))
+
 -- * Testing offsets
 
 -- | helper function
 parse :: FilePath -> IO [TcfElement]
-parse fName = runX (readDocument [withValidate no] fName >>>
-                    propagateNamespaces >>>
-                    multi (mkTcfElement [])
-                   )
+parse fName = do
+  lineOffsets <- runLineOffsetParser fName
+  parsed <- runXIOState (initialState lineOffsets)
+            (readDocument [withValidate no] fName >>>
+             propagateNamespaces >>>
+             multi (mkTcfElement [])
+            )
+  return parsed
 
 -- | Testing offsets of token with randomized number
 prop_textOffset :: Positive Int -> Property
@@ -82,76 +92,76 @@ test_ordinaryWord = do
     ["Hallo", "Welt"]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "Hallo Welt" 1 1)]
+    parsed = [(mkTcfTextSample "Hallo Welt" 1 1)]
 
 test_punctuation = do
   assertEqual
     ["Hallo", "Welt", "!"]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "Hallo Welt!" 1 1)]
+    parsed = [(mkTcfTextSample "Hallo Welt!" 1 1)]
 
 test_emptyString = do
   assertEqual
     []
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "" 1 1)]
+    parsed = [(mkTcfTextSample "" 1 1)]
 
 test_continuingWord = do
   assertEqual
     ["Hallo", "Welt"]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "Hal" 1 1),
-              (TcfText "lo Welt" 4 7)]
+    parsed = [(mkTcfTextSample "Hal" 1 1),
+              (mkTcfTextSample "lo Welt" 4 7)]
 
 test_continuingWordOverStruct = do
   assertEqual
     ["Hallo", "Welt"]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "Hal" 1 1),
+    parsed = [(mkTcfTextSample "Hal" 1 1),
               (TcfStructure (mkNsName "b" "http://www.w3.org/1999/xhtml") 4 10 4 16),
-              (TcfText "lo Welt" 4 7)]
+              (mkTcfTextSample "lo Welt" 4 7)]
 
 test_hyphenCase1 = do
   assertEqual
     ["Hallo", "Welt"]
     (map getToken (tokenize [(Hyphen '-')] parsed))
   where
-    parsed = [(TcfText "Hal-" 1 1),
-              (TcfText "\n\t " 4 7),
+    parsed = [(mkTcfTextSample "Hal-" 1 1),
+              (mkTcfTextSample "\n\t " 4 7),
               (TcfLineBreak),
-              (TcfText "lo Welt" 10 17)]
+              (mkTcfTextSample "lo Welt" 10 17)]
 
 test_hyphenCase1WithWhiteSpace = do
   assertEqual
     ["Hallo", "Welt"]
     (map getToken (tokenize [(Hyphen '-')] parsed))
   where
-    parsed = [(TcfText "Hal-" 1 1),
-              (TcfText "\n\t " 4 7),
+    parsed = [(mkTcfTextSample "Hal-" 1 1),
+              (mkTcfTextSample "\n\t " 4 7),
               (TcfLineBreak),
-              (TcfText "\n\t\t lo Welt" 10 17)]
+              (mkTcfTextSample "\n\t\t lo Welt" 10 17)]
 
 test_hyphenCase2 = do
   assertEqual
     ["Hallo", "Welt"]
     (map getToken (tokenize [(Hyphen '-')] parsed))
   where
-    parsed = [(TcfText "Hal-" 1 1),
+    parsed = [(mkTcfTextSample "Hal-" 1 1),
               (TcfLineBreak),
-              (TcfText "lo Welt" 4 7)]
+              (mkTcfTextSample "lo Welt" 4 7)]
 
 test_hyphenCase2WithWhiteSpace = do
   assertEqual
     ["Hallo", "Welt"]
     (map getToken (tokenize [(Hyphen '-')] parsed))
   where
-    parsed = [(TcfText "Hal-" 1 1),
+    parsed = [(mkTcfTextSample "Hal-" 1 1),
               (TcfLineBreak),
-              (TcfText "\n\tlo Welt" 4 7)]
+              (mkTcfTextSample "\n\tlo Welt" 4 7)]
 
 {- -- FIXME!
 test_hyphenCase2WithWhiteSpaceContinued = do
@@ -159,11 +169,11 @@ test_hyphenCase2WithWhiteSpaceContinued = do
     ["Hallo", "Welt"]
     (map getToken (tokenize [(Hyphen '-')] parsed))
   where
-    parsed = [(TcfText "Hal-" 1 1),
+    parsed = [(mkTcfTextSample "Hal-" 1 1),
               (TcfLineBreak),
-              (TcfText "\n\tl" 7 12),
+              (mkTcfTextSample "\n\tl" 7 12),
               (TcfStructure (mkNsName "b" "http://www.w3.org/1999/xhtml") 11 13 11 16), 
-              (TcfText "o Welt" 4 7)]
+              (mkTcfTextSample "o Welt" 4 7)]
 -}
 
 -- * Date tests
@@ -173,89 +183,89 @@ test_numDateOnly = do
     ["31.", "10.", "1517"]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "31. 10. 1517" 1 1)]  
+    parsed = [(mkTcfTextSample "31. 10. 1517" 1 1)]
 
 test_numDate = do
   assertEqual
     ["31.", "10.", "1517", "is", "a", "date", "."]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "31. 10. 1517 is a date." 1 1)]  
+    parsed = [(mkTcfTextSample "31. 10. 1517 is a date." 1 1)]
 
 test_numDateSentence = do
   assertEqual
     ["31.", "10.", "1517", "."]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "31. 10. 1517." 1 1)]  
+    parsed = [(mkTcfTextSample "31. 10. 1517." 1 1)]
 
 test_numDateContinued = do
   assertEqual
     ["31.", "10.", "1517", "is", "a", "date", "."]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "31." 1 1),
-              (TcfText " 10. 1517 is a date." 4 7)]  
-  
+    parsed = [(mkTcfTextSample "31." 1 1),
+              (mkTcfTextSample " 10. 1517 is a date." 4 7)]
+
 test_numDateLineBreak = do
   assertEqual
     ["31.", "10.", "1517", "is", "a", "date", "."]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "31." 1 1),
+    parsed = [(mkTcfTextSample "31." 1 1),
               (TcfLineBreak),
-              (TcfText "\n10. 1517 is a date." 4 7)]  
-  
+              (mkTcfTextSample "\n10. 1517 is a date." 4 7)]
+
 test_numLitMonthDate = do
   assertEqual
     ["31.", "Okt.", "1517", "is", "a", "date", "."]
     (map getToken (tokenize monthsConfig parsed))
   where
-    parsed = [(TcfText "31. Okt. 1517 is a date." 1 1)]  
+    parsed = [(mkTcfTextSample "31. Okt. 1517 is a date." 1 1)]
 
 test_numLitMonthDateLineBreak = do
   assertEqual
     ["31.", "Oktob.", "1517", "is", "a", "date", "."]
     (map getToken (tokenize monthsConfig parsed))
   where
-    parsed = [(TcfText "31." 1 1),
+    parsed = [(mkTcfTextSample "31." 1 1),
               (TcfLineBreak),
-              (TcfText "\nOktob. 1517 is a date." 4 7)]  
+              (mkTcfTextSample "\nOktob. 1517 is a date." 4 7)]
 
 test_litMonthOnly = do
   assertEqual
     ["im", "Okt.", "1517"]
     (map getToken (tokenize monthsConfig parsed))
   where
-    parsed = [(TcfText "im   Okt. 1517" 1 1)]
+    parsed = [(mkTcfTextSample "im   Okt. 1517" 1 1)]
 
 test_numLitMonthDateNoAbbrev = do
   assertEqual
     ["war", "am", "31.", "Oktober", ".", "1517", "war", "ein"]
     (map getToken (tokenize monthsConfig parsed))
   where
-    parsed = [(TcfText "war am 31. Oktober. 1517 war ein" 1 1)]  
+    parsed = [(mkTcfTextSample "war am 31. Oktober. 1517 war ein" 1 1)]
 
 test_invalidDay = do
   assertEqual
     ["41", ".", "Oktob.", "1517", "is", "no", "date", "."]
     (map getToken (tokenize monthsConfig parsed))
   where
-    parsed = [(TcfText "41. Oktob. 1517 is no date." 1 1)]  
+    parsed = [(mkTcfTextSample "41. Oktob. 1517 is no date." 1 1)]
 
 test_invalidNumMonth = do
   assertEqual
     ["31", ".", "13", ".", "1517", "is", "no", "date", "."]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "31. 13. 1517 is no date." 1 1)]  
+    parsed = [(mkTcfTextSample "31. 13. 1517 is no date." 1 1)]
 
 test_invalidLitMonth = do
   assertEqual
     ["31", ".", "Octop", ".", "1517", "is", "no", "date", "."]
     (map getToken (tokenize monthsConfig parsed))
   where
-    parsed = [(TcfText "31. Octop. 1517 is no date." 1 1)]  
+    parsed = [(mkTcfTextSample "31. Octop. 1517 is no date." 1 1)]
 
 
 -- More abbreviations
@@ -265,18 +275,18 @@ test_abbrevNoSpaces = do
     ["etwas", "Ausgedehntes", ",", "z.", "B.", "eine", "Wolke"]
     (map getToken (tokenize [(Abbrev1CharToken True)] parsed))
   where
-    parsed = [(TcfText "etwas Ausgedehntes, z.B. eine Wolke" 1 1)]  
+    parsed = [(mkTcfTextSample "etwas Ausgedehntes, z.B. eine Wolke" 1 1)]
 
 test_confAbbrevs = do
   assertEqual
     ["Prof.", "Rammler"]
     (map getToken (tokenize [(Abbreviation "Prof")] parsed))
   where
-    parsed = [(TcfText "Prof. Rammler" 1 1)]  
+    parsed = [(mkTcfTextSample "Prof. Rammler" 1 1)]
 
 test_confNoAbbrevs = do
   assertEqual
     ["Prof", ".", "Rammler"]
     (map getToken (tokenize [] parsed))
   where
-    parsed = [(TcfText "Prof. Rammler" 1 1)]  
+    parsed = [(mkTcfTextSample "Prof. Rammler" 1 1)]
